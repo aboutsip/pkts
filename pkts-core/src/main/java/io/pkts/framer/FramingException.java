27a847eb2c258c9cfae1c464bed75a6ee632943b
Jonas Borjesson <jonas@aboutsip.com>
2017-10-21 10:57:56 -0700

    Changing FramingException to be a RuntimeException

--- pkts-core/src/main/java/io/pkts/framer/FramingException.java
+++ pkts-core/src/main/java/io/pkts/framer/FramingException.java
@@ -2,10 +2,10 @@ package io.pkts.framer;
 
 import io.pkts.protocol.Protocol;
 
-public class FramingException extends Exception {
+public class FramingException extends RuntimeException {
   private final Protocol protocol;
 
-  FramingException(String message, Protocol protocol) {
+  FramingException(final String message, final Protocol protocol) {
     super(message);
     this.protocol = protocol;
   }
