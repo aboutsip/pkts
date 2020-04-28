80ba62aa41f2c6b7e6e7257c31286f6fb16e08ed
nragon <Nunoo_7@hotmail.com>
2020-04-28 21:44:03 +0100

    Basic tests for raw sip framer

--- pkts-core/src/test/java/io/pkts/frame/SipFrameTest.java
+++ pkts-core/src/test/java/io/pkts/frame/SipFrameTest.java
@@ -3,27 +3,27 @@
  */
 package io.pkts.frame;
 
-import io.pkts.PktsTestBase;
-import io.pkts.buffer.Buffers;
-import io.pkts.framer.SIPFramer;
-import io.pkts.packet.IPPacket;
-import io.pkts.packet.IPv4Packet;
-import io.pkts.packet.TransportPacket;
-import io.pkts.packet.sip.SipPacket;
-import io.pkts.packet.sip.SipRequestPacket;
-import io.pkts.protocol.Protocol;
+
+
+
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.File;
-import java.util.List;
-
-import static junit.framework.Assert.assertEquals;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import io.pkts.PktsTestBase;
+import io.pkts.buffer.Buffers;
+import io.pkts.framer.SIPFramer;
+import io.pkts.packet.IPPacket;
+import io.pkts.packet.TransportPacket;
+import io.pkts.packet.sip.SipPacket;
+import io.pkts.packet.sip.SipRequestPacket;
 
 /**
  * @author jonas@jonasborjesson.com
@@ -43,7 +43,6 @@ public class SipFrameTest extends PktsTestBase {
         transportPkt = mock(TransportPacket.class);
         ipPkt = mock(IPPacket.class);
         when(transportPkt.getParentPacket()).thenReturn(ipPkt);
-
         when(transportPkt.getArrivalTime()).thenReturn(123L);
         when(ipPkt.getSourceIP()).thenReturn("192.168.0.100");
         when(ipPkt.getDestinationIP()).thenReturn("10.36.10.10");
@@ -64,13 +63,7 @@ public class SipFrameTest extends PktsTestBase {
     public void testParseSipRequest() throws Exception {
         final SIPFramer framer = new SIPFramer();
         final SipPacket sip = framer.frame(transportPkt, sipFrameBuffer);
-        assertThat(sip.getMethod().toString(), is("INVITE"));
-        assertThat(((SipRequestPacket) sip).getRequestUri().toString(), is("sip:service@127.0.0.1:5090"));
-
-        // just check some random headers
-        assertThat(sip.getHeader(Buffers.wrap("Content-Length")).get().getValue().toString(), is("129"));
-        assertThat(sip.getHeader(Buffers.wrap("Call-ID")).get().getValue().toString(), is("1-16732@127.0.1.1"));
-
+        testSipRequest(sip);
         assertThat(sip.getArrivalTime(), is(123L));
         assertThat(((IPPacket) sip.getParentPacket().getParentPacket()).getSourceIP(), is("192.168.0.100"));
         assertThat(((IPPacket) sip.getParentPacket().getParentPacket()).getDestinationIP(), is("10.36.10.10"));
@@ -81,10 +74,52 @@ public class SipFrameTest extends PktsTestBase {
     @Test
     public void testParseSipResponse() throws Exception {
         final SIPFramer framer = new SIPFramer();
-        final SipPacket sip = framer.frame(transportPkt, sipFrameBuffer180Response);
+        testSipResponse(framer.frame(transportPkt, sipFrameBuffer180Response));
+    }
+
+    @Test
+    public void testParseSipRequestRaw() throws Exception {
+        final SIPFramer framer = new SIPFramer();
+        final SipPacket sip = framer.frame(null, sipFrameBuffer);
+        assertNull(sip.getParentPacket());
+        testSipRequest(sip);
+    }
+
+    @Test
+    public void testParseSipResponseRaw() throws Exception {
+        final SIPFramer framer = new SIPFramer();
+        final SipPacket sip = framer.frame(null, sipFrameBuffer180Response);
+        assertNull(sip.getParentPacket());
+        testSipResponse(sip);
+    }
+
+    /**
+     * Tests basic sip response content
+     *
+     * @param sip
+     *            sip packet from {@link SIPFramer}
+     */
+    @SuppressWarnings("OptionalGetWithoutIsPresent")
+    private static void testSipResponse(final SipPacket sip) {
+
         assertThat(sip.getMethod().toString(), is("INVITE"));
         assertThat(sip.getHeader(Buffers.wrap("Call-ID")).get().getValue().toString(), is("1-16732@127.0.1.1"));
         assertThat(sip.getHeader(Buffers.wrap("Content-Length")).get().getValue().toString(), is("0"));
     }
 
+    /**
+     * Tests basic sip request content
+     *
+     * @param sip
+     *            sip packet from {@link SIPFramer}
+     */
+    private static void testSipRequest(final SipPacket sip) {
+
+        assertThat(sip.getMethod().toString(), is("INVITE"));
+        assertThat(((SipRequestPacket) sip).getRequestUri().toString(), is("sip:service@127.0.0.1:5090"));
+
+        // just check some random headers
+        assertThat(sip.getHeader(Buffers.wrap("Content-Length")).get().getValue().toString(), is("129"));
+        assertThat(sip.getHeader(Buffers.wrap("Call-ID")).get().getValue().toString(), is("1-16732@127.0.1.1"));
+    }
 }
